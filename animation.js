74d4c3916b9aaaff84c063f8f648ecfc0b95a812
Pedro Souza <peedrosouz6@gmail.com>
2022-04-21 14:03:42 -0300

    update

---
+++ animation.js
@@ -0,0 +1,18 @@
+const target = document.querySelectorAll('[data-anima]');
+
+if(target.length) {
+    window.addEventListener('scroll', () => {
+        animaScroll();
+    })
+}
+
+function animaScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach((element) => {
+        if(windowTop > element.offsetTop) {
+            element.classList.add('animation');
+        } else {
+            element.classList.remove('animation');
+        }
+    })
+}
--- menu.js
+++ menu.js
@@ -19,25 +19,3 @@ menuOpen.addEventListener('click', () => {
 menuClose.addEventListener('click', () => {
     navHeader.classList.remove('active');
 });
-
-
-
-//------------------------------animation------------------------------
-const target = document.querySelectorAll('[data-anima]');
-
-if(target.length) {
-    window.addEventListener('scroll', () => {
-        animaScroll();
-    })
-}
-
-function animaScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-    target.forEach((element) => {
-        if(windowTop > element.offsetTop) {
-            element.classList.add('animation');
-        } else {
-            element.classList.remove('animation');
-        }
-    })
-}
